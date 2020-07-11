3c6b061d28c1cc0af740090e2caf8c1c62274e18
RajanPatel97 <rajan.n.patel.97@gmail.com>
2020-07-11 17:18:26 +0100

    connected database

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const db = knex({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
+    user : 'postgres',
+    password : '', //add password when required
+    database : 'smartbrain'
   }
 });
 
