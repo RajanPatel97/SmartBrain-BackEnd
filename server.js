23bd3e4d6d054e5f0840eb0676661930a7bb0bd9
RajanPatel97 <rajan.n.patel.97@gmail.com>
2020-07-12 14:04:29 +0100

    updated env port

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=> {
+  console.log(`app is running on port ${process.env.PORT}`);
 })
