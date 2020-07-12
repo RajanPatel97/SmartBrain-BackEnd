63a7eb7a486700a2df775b69edbaaeac73c0418c
RajanPatel97 <rajan.n.patel.97@gmail.com>
2020-07-12 15:06:02 +0100

    updated host

--- server.js
+++ server.js
@@ -12,8 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    connectionString : process.env.DATABASE_URL,
-    ssl: true
+    connectionString : process.env.DATABASE_URL
   }
 });
 
