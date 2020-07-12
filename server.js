0b1e974bba7acad6505d30ce0ead68a397fd508b
RajanPatel97 <rajan.n.patel.97@gmail.com>
2020-07-12 14:09:09 +0100

    changed response

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> {res.send(db.users)})
+app.get('/', (req, res)=> {res.send('it is working')})
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
