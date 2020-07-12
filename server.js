2319bd3c6e75819d753ed0789a12704c621e4f6a
RajanPatel97 <rajan.n.patel.97@gmail.com>
2020-07-12 14:51:22 +0100

    added new host

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'Synergy19', //add password when required
-    database : 'smartbrain'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
